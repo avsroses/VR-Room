29cb8434e4ba8bc9c28bc6b6aab704d33362571a
avsroses <ava.wilkinson@student.norwichuni.ac.uk>
2025-02-06 12:20:19 +0000

    bubbles

--- Assets/Scripts/CustomBubbles.cs
+++ Assets/Scripts/CustomBubbles.cs
@@ -4,15 +4,19 @@ public class CustomBubbles : MonoBehaviour
 {
     [Tooltip("The object that will be spawned")]
     public GameObject prefab = null;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    [Tooltip("The transform where the object is spawned")]
+    public Transform spawnTransform = null;
+    
+    public void Spawn() {
+        //create bubble
+        GameObject bubble = Instantiate(prefab, spawnTransform.position, spawnTransform.rotation);
+        //rotate randomly
+        bubble.transform.rotation = Random.rotation;
+        //set randomised launch speed
+        float launchSpeed = Random.Range(0.5f, 1.5f);
+        //create Vector3 to represent forawrd force
+        Vector3 force = spawnTransform.forward * launchSpeed;
+        bubble.GetComponent<Rigidbody>().AddForce(force);
+        bubble.GetComponent<Rigidbody>().AddTorque(new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)));
     }
 }
