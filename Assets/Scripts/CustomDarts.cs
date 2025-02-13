827d582acb1a44e3587fa7dce577d4ce9ad96948
avsroses <ava.wilkinson@student.norwichuni.ac.uk>
2025-02-13 11:09:23 +0000

    dart interaction

--- Assets/Scripts/CustomDarts.cs
+++ Assets/Scripts/CustomDarts.cs
@@ -11,7 +11,7 @@ public class CustomDarts : MonoBehaviour
         //create dart
         GameObject dart = Instantiate(prefab, spawnTransform.position, spawnTransform.rotation);
         //set launch speed
-        float launchSpeed = 1.5f;
+        float launchSpeed = 5f;
         //create Vector3 to represent forawrd force
         Vector3 force = spawnTransform.forward * launchSpeed;
         dart.GetComponent<Rigidbody>().AddForce(force);
